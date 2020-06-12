00a8e7fdff556c4393278c9ab567212d57aeed15
shintaro <ghsprskob97@yahoo.co.jp>
2020-06-12 14:10:11 +0000

    post

---
+++ app/Micropost.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Micropost extends Model
+{
+    protected $fillable = ['content'];
+
+    /**
+     * この投稿を所有するユーザ。（ Userモデルとの関係を定義）
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    
+        /**
+     * このユーザに関係するモデルの件数をロードする。
+     */
+    public function loadRelationshipCounts()
+    {
+        $this->loadCount('microposts');
+    }
+
+}
+
